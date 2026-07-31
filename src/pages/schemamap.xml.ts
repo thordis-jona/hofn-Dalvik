43cd2649646fb93893154a21e3c3dfd0dc0da674
Jökull Sólberg <jokull@solberg.is>
2026-07-31 18:10:23 +0000

    Use Worker-safe JSON-LD endpoint URLs

--- src/pages/schemamap.xml.ts
+++ src/pages/schemamap.xml.ts
@@ -7,7 +7,7 @@ const lastModified = new Date('2026-07-31T00:00:00.000Z');
 export const GET = createSchemaMap({
   siteUrl: 'https://hofn.manifesto.is',
   entries: [
-    { path: '/schema/site', lastModified },
-    { path: '/schema/guide', lastModified },
+    { path: '/schema/site.jsonld', lastModified },
+    { path: '/schema/guide.jsonld', lastModified },
   ],
 });
